9be6ce653872e41bb80f798478945d56ec58b5f5
wr5gk <wr5gk@mathcs02.richmond.edu>
2018-04-25 14:29:54 -0400

    Added animation code.

---
+++ VehicleBase.cpp
@@ -0,0 +1,13 @@
+#ifndef __VEHICLE_BASE_CPP__
+#define __VEHICLE_BASE_CPP__
+
+#include "VehicleBase.h"
+
+int VehicleBase::vehicleCount = 0;
+
+VehicleBase::VehicleBase(VehicleBase::VehicleType type)
+    : vehicleID(VehicleBase::vehicleCount++), vehicleType(type) {}
+
+VehicleBase::~VehicleBase() {}
+
+#endif
