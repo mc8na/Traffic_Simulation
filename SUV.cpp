7b9b84c34348b1b7ced03c54570d6e3354dbe564
wr5gk <wr5gk@mathcs02.richmond.edu>
2018-04-27 18:37:02 -0400

    Segmentation Faults

--- SUV.cpp
+++ SUV.cpp
@@ -17,32 +17,32 @@ SUV::SUV(Section& sec, Lane::Direction dir) : Vehicle(sec, dir, VehicleBase::SUV
 
 std::vector<Section*> SUV::proceed(Lane& lane)
 {
-	if (location.front() == NULL) // if front of vehicle has exited the lane
+	if (location.front() == nullptr) // if front of vehicle has exited the lane
 	{
 		(*location.back()).leave(); // move back forward
 
-		if (location.at(1) != NULL) // if vehicle has fully exited the lane
+		if (location.at(1) != nullptr) // if vehicle has fully exited the lane
 		{
-			location.back() = NULL;
+			location.back() = nullptr;
 			lane.removeVehicle(); // remove vehicle from lane
 		}
 		else // if part of vehicle still in the lane
 		{
 			location.back() = location.at(1); // advance back Section
-			location.at(1) = NULL;
+			location.at(1) = nullptr;
 		}
 	}
-	else if ((*location.front()).getNext(vDirection) == NULL) // if vehicle about to exit lane
+	else if ((*location.front()).getNext(vDirection) == nullptr) // if vehicle about to exit lane
 	{ 
 		(*location.back()).leave();
 		location.back() = location.at(1);
 		location.at(1) = location.front();
-		location.front() = NULL;
+		location.front() = nullptr;
 
 	}
 	if((*((*location.front()).getNext(vDirection))).isOpen() == true) // if next Section is open
 	{
-		if(location.back() != NULL && location.back() != location.at(1))
+		if(location.back() != nullptr && location.back() != location.at(1))
 		{
 			(*location.back()).leave(); // back sets the Section it leaves to open
 		}
